32189829c093f137daa17c1f03c7ef40a408f6f3
KogaMasamichi <km.060928@gmail.com>
2021-01-16 12:56:00 +0900

    タイトル表示

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from 'react'
 
 const App = () => {
     return ( <
-        React.Fragment >
-        <
-        h1 > Hello, World! < /h1> <
-        Form / >
-        <
-        /React.Fragment>
+        h1 > TODO APP < /h1> 
     )
 }
 
