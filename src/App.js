e9b35c15ea2977350ea09f36d792776e4da2d94f
KogaMasamichi <km.060928@gmail.com>
2021-01-16 11:50:14 +0900

    予習

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+    return ( <
+        React.Fragment >
+        <
+        h1 > Hello, World! < /h1> <
+        Form / >
+        <
+        /React.Fragment>
+    )
+}
+
+export default App
---
+++ src/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Form = () => {
+    return ( <
+        form >
+        <
+        input type = 'text' / >
+        <
+        /form>
+    )
+}
+
+export default Form
